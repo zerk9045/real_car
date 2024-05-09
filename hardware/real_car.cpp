43c134cb41243f453ca9e0af0f39400bd51bbc16
rbretmounet <mrquacksxd@gmail.com>
2024-05-08 19:15:49 -0700

    Updated publishing speed to 10ms

--- hardware/real_car.cpp
+++ hardware/real_car.cpp
@@ -22,12 +22,16 @@ namespace real_car
 HardwareCommandPubMotor::HardwareCommandPubMotor() : Node("motor_publisher")
 {
   motor_publisher_ = this->create_publisher<std_msgs::msg::String>("pi_motor_publishing_topic", 10);
+    timer_ = this->create_wall_timer(
+            10ms, std::bind(&MinimalPublisher::timer_callback, this));
 }
 
 // Create the topic that the Pi will publish to and Pico will subscribe to
 HardwareCommandPubServo::HardwareCommandPubServo() : Node("servo_publisher")
 {
   servo_publisher_ = this->create_publisher<std_msgs::msg::String>("pi_servo_publishing_topic", 10);
+    timer_ = this->create_wall_timer(
+            10ms, std::bind(&MinimalPublisher::timer_callback, this));
 }
 
 // Function for publishing to the topic that the Pico will subscribe to
