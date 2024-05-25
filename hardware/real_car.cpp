338d274d03da5caca07bf0b0c41f403816419cd4
Adrien Salvador <adrienjoelsalvador@gmail.com>
2024-05-24 18:40:58 -0700

    Fixed error

--- hardware/real_car.cpp
+++ hardware/real_car.cpp
@@ -391,7 +391,7 @@ hardware_interface::return_type real_car ::RealCarHardware::write(
   // }
 
   // publish the motor PWM and direction
-  motor_pub_->publishSpeed(normalizedSpeed);
+  motor_pub_->publishSpeed(hw_interfaces_["traction"].command.velocity);
 
   // convert the normalizedAngle into servo PWM
   servoVelToPWM(normalizedAngle, servoPWM);
